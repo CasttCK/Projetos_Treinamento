a3d86abdd7e138889e91f8189c40e00a3c77ca5e
CasttCK <vini.kronemberger@gmail.com>
2023-09-29 17:14:33 -0300

    Criação jogo adivinhação com variáveis e loop

---
+++ Treinamento_Python/Jogos/adivinhacao.py
@@ -0,0 +1,28 @@
+print("*****************************")
+print("Este é um jogo de adivinhação")
+print("*****************************")
+
+numero_oculto = 35
+numero_tentativas = 5
+
+while numero_tentativas > 0:
+    if(numero_tentativas == 1):
+        print("\nEssa é a sua última tentativa. Pense bem!")
+    else:
+        print("\nVocê tem ", numero_tentativas, "tentativas restantes")
+
+    palpite = int(input("Digite um palpite: "))
+    acertou = palpite == numero_oculto
+    maior = palpite > numero_oculto
+    menor = palpite < numero_oculto
+
+    if(palpite == numero_oculto):
+        vitoria = True
+        print("Parabéns, você acertou!!")
+    else:
+        if(maior):
+            print("O número escolhido é maior do que o segredo!")
+        elif(menor):
+            print("O número escolhido é menor do que o segredo!")
+
+    numero_tentativas -= 1
