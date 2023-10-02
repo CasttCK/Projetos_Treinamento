6835344d28955e1c3f8315c7fd3700391db34fcc
CasttCK <vini.kronemberger@gmail.com>
2023-10-02 17:30:56 -0300

    Implementação de pontuação do jogo

--- Treinamento_Python/Jogos/adivinhacao.py
+++ Treinamento_Python/Jogos/adivinhacao.py
@@ -12,23 +12,22 @@ while True:
     except ValueError:
         print("Você digitou um valor errado.\nEscolha uma opção\n")
 
-numero_oculto = 0
-numero_tentativas = 5
-tentativas_restantes = 5
-quantidade_numeros = 0
+numero_oculto = quantidade_numeros = pontuacao_geral = pontuacao_possivel = 0
+numero_tentativas = tentativas_restantes = 5
 
 match(dificuldade):
     case 1:
         quantidade_numeros = 10
         numero_oculto = random.randrange(1, 11)
+        pontuacao_geral = pontuacao_possivel = 100
     case 2:
         quantidade_numeros = 50
         numero_oculto = random.randrange(1, 51)
+        pontuacao_geral = pontuacao_possivel = 500
     case 3:
         quantidade_numeros = 100
         numero_oculto = random.randrange(1, 101)
-
-print(numero_oculto)
+        pontuacao_geral = pontuacao_possivel = 1000
 
 for rodada in range(numero_tentativas):
     if(tentativas_restantes == 1):
@@ -48,12 +47,16 @@ for rodada in range(numero_tentativas):
     if(palpite == numero_oculto):
         vitoria = True
         print("Parabéns, você acertou!!")
+        print("Você fez {0} pontos de {1} possíveis".format(pontuacao_geral, pontuacao_possivel))
         break
     else:
         if(maior):
             print("O número escolhido é maior do que o segredo!")
         elif(menor):
             print("O número escolhido é menor do que o segredo!")
+
+        pontos_perdidos_rodada = abs(numero_oculto - palpite)
+        pontuacao_geral -= pontos_perdidos_rodada
 
     tentativas_restantes -= 1
 
