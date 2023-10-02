ce28a623ce528097c820dbb5da916418d03e261f
CasttCK <vini.kronemberger@gmail.com>
2023-10-02 11:49:15 -0300

    Novas possibilidades .format()

--- Treinamento_Python/Jogos/adivinhacao.py
+++ Treinamento_Python/Jogos/adivinhacao.py
@@ -26,3 +26,14 @@ while numero_tentativas > 0:
             print("O número escolhido é menor do que o segredo!")
 
     numero_tentativas -= 1
+
+# FUNÇÃO .format()
+# Você consegue usar a função para a interpolação passando o index do valor caso tenha mais de um parâmetro.
+# Consegue também usar alguns parâmetros para trabalhar com números float conforme abaixo:
+# {:f} - informa que é uma formatação float | {:.2} - Trabalha com duas casas depois da vírgula
+# {:7.2f} - o número depois dos dois pontos informa quantos caracteres existe e vai preencher com espaços o que faltar
+# {:07.2f} - a mesma coisa de cima, porém vai preencher com '0' o que faltar.
+
+# Pode trabalhar com inteiros usando o d (digital) também da seguinte forma. O exemplo abaixo formata uma data:
+# "Data: {:02d}/{:02d}/{:04d}".format(1, 6, 1996)
+# O resultado será: Data: 01/06/1996
