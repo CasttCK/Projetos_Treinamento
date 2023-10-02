b38b2de4023ee257341ecf7a5c4fce869fb319f6
CasttCK <vini.kronemberger@gmail.com>
2023-10-02 14:33:40 -0300

    Usando Break e Continue

--- Treinamento_Python/Jogos/adivinhacao.py
+++ Treinamento_Python/Jogos/adivinhacao.py
@@ -5,27 +5,32 @@ print("*****************************")
 numero_oculto = 35
 numero_tentativas = 5
 
-while numero_tentativas > 0:
+for rodada in numero_tentativas + 1:
     if(numero_tentativas == 1):
         print("\nEssa é a sua última tentativa. Pense bem!")
     else:
         print("\nVocê tem {} tentativas restantes".format(numero_tentativas))
 
-    palpite = int(input("Digite um palpite: "))
+    palpite = int(input("Digite um número entre 1 e 100: "))
     acertou = palpite == numero_oculto
     maior = palpite > numero_oculto
     menor = palpite < numero_oculto
 
+    if(palpite < 1 or palpite > 100):
+        print("Você digitou um número errado e perdeu uma rodada")
+        continue
+
     if(palpite == numero_oculto):
         vitoria = True
         print("Parabéns, você acertou!!")
+        break
     else:
         if(maior):
             print("O número escolhido é maior do que o segredo!")
         elif(menor):
             print("O número escolhido é menor do que o segredo!")
 
-    numero_tentativas -= 1
+print("Fim de jogo!")
 
 # FUNÇÃO .format()
 # Você consegue usar a função para a interpolação passando o index do valor caso tenha mais de um parâmetro.
