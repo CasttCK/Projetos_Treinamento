bd396d6a10e07d3afe99bcf880531c727bfba516
CasttCK <vini.kronemberger@gmail.com>
2023-10-24 10:27:47 -0300

    Update forca.py

--- TreinamentoPython/Jogos/forca.py
+++ TreinamentoPython/Jogos/forca.py
@@ -4,7 +4,8 @@ def jogar():
     print("*****************************")
 
     palavra_secreta = ""
-    layout_inicial = ""
+    layout_inicial = []
+    tentativas = 7
     
     enforcou = False
     acertou = False
@@ -12,26 +13,32 @@ def jogar():
     palavra_secreta = input("Digite uma palavra para começar o jogo")
 
     for letra in palavra_secreta:
-        layout_inicial += "_"
+        layout_inicial.append("_")
 
+    print(layout_inicial)
     print("A palavra secreta tem {0} letras".format(len(palavra_secreta)))
 
     while(not enforcou and not acertou):
         index = 0
         letra_palpite = input("Digite uma letra")
+        tentativas -= 1
         letra_palpite = letra_palpite.strip()
         letra_palpite = letra_palpite.lower()
 
         for letra in palavra_secreta:
             if(letra == letra_palpite):
                 layout_inicial[index] = letra_palpite
-                index += 1
 
-                print(layout_inicial)
+            index -= 1
 
+        print(layout_inicial)
+        acertou = "_" not in layout_inicial
+        enforcou = tentativas < 0
 
-
-    print("Fim do jogo")
+    if(acertou):
+        print("Você acertou, parabéns! \nFim do jogo")
+    else:
+        print("Fim do jogo. \nVocê perdeu ! -_-")
 
 if(__name__ == "__main__"):
     jogar()
