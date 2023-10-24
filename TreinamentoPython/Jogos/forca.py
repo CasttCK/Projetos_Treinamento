840bd59d5812524e3a6a9cccbc710b36a0106788
CasttCK <vini.kronemberger@gmail.com>
2023-10-24 14:20:21 -0300

    Feat: Finalização jogo da forca

--- TreinamentoPython/Jogos/forca.py
+++ TreinamentoPython/Jogos/forca.py
@@ -1,22 +1,17 @@
-def jogar():
-    print("*****************************")
-    print("** Este é um jogo de forca **")
-    print("*****************************")
+import random
 
-    palavra_secreta = ""
-    layout_inicial = []
+def jogar():
+    imprime_saudacoes("Este é um jogo de forca")
+
+    palavra_secreta = selecionar_palavra()
+    layout_inicial = criar_template_forca(palavra_secreta)
     tentativas = 7
     
     enforcou = False
     acertou = False
 
-    palavra_secreta = input("Digite uma palavra para começar o jogo")
-
-    for letra in palavra_secreta:
-        layout_inicial.append("_")
-
+    print("\nA palavra secreta tem {0} letras".format(len(palavra_secreta)))
     print(layout_inicial)
-    print("A palavra secreta tem {0} letras".format(len(palavra_secreta)))
 
     while(not enforcou and not acertou):
         index = 0
@@ -29,7 +24,7 @@ def jogar():
             if(letra == letra_palpite):
                 layout_inicial[index] = letra_palpite
 
-            index -= 1
+            index += 1
 
         print(layout_inicial)
         acertou = "_" not in layout_inicial
@@ -40,5 +35,32 @@ def jogar():
     else:
         print("Fim do jogo. \nVocê perdeu ! -_-")
 
+def imprime_saudacoes(mensagem):
+    print("*****************************")
+    print("** {} **".format(mensagem))
+    print("*****************************")
+
+def selecionar_palavra():
+    palavras_disponiveis = []
+    arquivo = open("palavras.txt", "r")
+
+    for linha in arquivo:
+        palavras_disponiveis.append(linha.strip())
+
+    arquivo.close()
+
+    index_aleatorio = random.randrange(0, len(palavras_disponiveis))
+    palavra_secreta = palavras_disponiveis[index_aleatorio]
+
+    return palavra_secreta
+
+def criar_template_forca(palavra):
+    layout = []
+    for letra in palavra:
+        layout.append("_")
+
+    return layout
+
 if(__name__ == "__main__"):
     jogar()
+
