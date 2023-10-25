0b1a40076d887aae99560fa4c91b7f0804ea7f94
CasttCK <vini.kronemberger@gmail.com>
2023-10-24 21:48:46 -0300

    Feat: Melhorias site com Flask

--- TreinamentoPython/jogoteca/jogoteca.py
+++ TreinamentoPython/jogoteca/jogoteca.py
@@ -1,14 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
+class Jogo:
+    def __init__(self, nome, categoria, console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
+
+jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
+jogo2 = Jogo('Counter-Strike', 'FPS', 'PC')
+jogo3 = Jogo('God of war', 'Rack n Slash', 'PS2')
+jogos = [jogo1, jogo2, jogo3]
 
 app = Flask(__name__)
 
-@app.route('/home')
-def main():
-    jogos = ['Tetris', 'Crash', 'Spider-Man']
-    return render_template('lista.html',
-                           titulo_pagina = 'Jogoteca',
-                           titulo_tabela = 'Jogos',
-                           jogos = jogos)
+@app.route('/')
+def index():
+    return render_template('lista.html', titulo = 'Jogos', jogos = jogos)
 
-app.run()
+@app.route('/novo')
+def novo_jogo():
+    return render_template('novo_jogo.html', titulo = 'Novo jogo')
+
+@app.route('/criar', methods=['POST',])
+def criar_novo_jogo():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+
+    novo_jogo = Jogo(nome, categoria, console)
+    jogos.append(novo_jogo)
+
+    return redirect('/')
+
+app.run(debug=True)
